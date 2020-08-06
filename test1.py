3a4ced13f55f33f27d9b12b8a55c3409c36591da
Santiago Davila <davilag.sj@gmail.com>
2020-08-06 15:45:05 -0300

    Fist attempt for fuctioning algorithm

--- test1.py
+++ test1.py
@@ -1,27 +1,52 @@
-from datetime import timedelta 
-
-def minutes_to_seconds(self, seg):
-    self.seg = seg
-    mins = datetime.timedelta.min(seconds=self.seg)
-    
-    return mins
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+  
 
 class Capacidad_aerobica: 
 
-    def __init__(self, kg, edad, sexo, tiempo):
-        pass
+    def __init__(self, kg, edad, sexo, mins, fre_car_pm):
 
- 
+        self.kg = kg
+        self.edad = edad
+        self.sexo = sexo
+        self.mins = mins
+        self.fre_car_pm = fre_car_pm
 
+    def first_part(self, kg):
+        logger.info('Inicialization first part of the ecuation')
+        
+        first = 132.6 - 0.17 * self.kg
+        print(first)
+        return first
 
+    def second_part(self, edad, sexo):
+        logger.info('Inicialization second part of the ecuation')
+
+        second = 0.39 * self.edad + 6.31 * self.sexo
+        print(second)
+        return second 
+    
+    def third_part(self, mins, fre_car_pm):
+        logger.info('Inicialization third part of the ecuation')
+
+        third = 3.27 * self.mins - 0.156 * self.fre_car_pm
+        print(third)
+        return third
+
+def main():
+
+    cap_aer = Capacidad_aerobica(50, 23, 1, 3, 110)
+    a = cap_aer.first_part(50)
+    b = cap_aer.second_part(23, 1)
+    c = cap_aer.third_part(3, 110)
+
+    return print(a - b - c)    
+  
 
 if __name__ == "__main__":
     
-    seg = 
-    fre_car_pm = fre_car_ps * 6 #frecuencia cardiaca por segundo a FC por min
-    kg = 10
-    sexo = 1
-    edad = 22
+    main()
 
- Cap_aer = 132,6 – (0,17 x kg) – (0,39 x edad) + (6,31 x sexo) – (3,27 x min) – (0,156 x fre_car_pm)
+
 
