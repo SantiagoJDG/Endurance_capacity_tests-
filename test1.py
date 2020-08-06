db14eedd04d90ca07f1a1dbf7eb279c303ed2a01
Santiago Davila <davilag.sj@gmail.com>
2020-08-06 13:13:47 -0300

    First commit for e10 project

---
+++ test1.py
@@ -0,0 +1,27 @@
+from datetime import timedelta 
+
+def minutes_to_seconds(self, seg):
+    self.seg = seg
+    mins = datetime.timedelta.min(seconds=self.seg)
+    
+    return mins
+
+class Capacidad_aerobica: 
+
+    def __init__(self, kg, edad, sexo, tiempo):
+        pass
+
+ 
+
+
+
+if __name__ == "__main__":
+    
+    seg = 
+    fre_car_pm = fre_car_ps * 6 #frecuencia cardiaca por segundo a FC por min
+    kg = 10
+    sexo = 1
+    edad = 22
+
+ Cap_aer = 132,6 – (0,17 x kg) – (0,39 x edad) + (6,31 x sexo) – (3,27 x min) – (0,156 x fre_car_pm)
+
